e943f9536b46ee0b721d36bb1e4036a89a4a2932
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-06-10 13:45:36 +0700

    updae lan 3

--- may_tinh.js
+++ may_tinh.js
@@ -2,23 +2,23 @@ const firstnumber = document.getElementById("firstnumber");
 const secondnumber = document.getElementById("secondnumber");
 
 function addnumbers() {
-    const addnuber = parseFloat(firstnumber.value) + parseFloat(secondnumber.value);
-    document.getElementById("addnumber").textContent = addnuber;
+    const addnumber = parseFloat(firstnumber.value) + parseFloat(secondnumber.value);
+    document.getElementById("result").textContent = addnumber;
 }
 function subtractnumbers() {
     const subtract =parseFloat(firstnumber.value) - parseFloat(secondnumber.value);
-    document.getElementById("subtractnumber").textContent = subtract;
+    document.getElementById("result").textContent = subtract;
 }
 function multiplication() {
     const multiplication = parseFloat(firstnumber.value) * parseFloat(secondnumber.value);
-    document.getElementById("multiplication").textContent = multiplication;
+    document.getElementById("result").textContent = multiplication;
 }
 function divide() {
     const divide = parseFloat(firstnumber.value) / parseFloat(secondnumber.value);
-    document.getElementById("divide").textContent = divide;
+    document.getElementById("result").textContent = divide;
 }
 
-document.getElementById("btnaddnumber").addEventListener("click", subtractnumbers);
-document.getElementById("btnsubtractnumber").addEventListener("click", addnumbers);
+document.getElementById("btnaddnumber").addEventListener("click", addnumbers);
+document.getElementById("btnsubtractnumber").addEventListener("click", subtractnumbers);
 document.getElementById("btnmultiplication").addEventListener("click", multiplication);
 document.getElementById("btndivide").addEventListener("click", divide);
