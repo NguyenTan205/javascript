c6b55b7e759916197bfb904b57406bbb35b7aaae
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-06-10 09:31:03 +0700

    updae lan 2

--- may_tinh.js
+++ may_tinh.js
@@ -9,6 +9,16 @@ function subtractnumbers() {
     const tru =parseFloat(first.value) - parseFloat(second.value);
     document.getElementById("tru").textContent = tru;
 }
+function multiplication() {
+    const nhan = parseFloat(first.value) * parseFloat(second.value);
+    document.getElementById("nhan").textContent = nhan;
+}
+function divide() {
+    const chia = parseFloat(first.value) / parseFloat(second.value);
+    document.getElementById("chia").textContent = chia;
+}
 
 document.getElementById("btntru").addEventListener("click", subtractnumbers);
 document.getElementById("btncong").addEventListener("click", addnumbers);
+document.getElementById("btnnhan").addEventListener("click", multiplication);
+document.getElementById("btnchia").addEventListener("click", divide);
